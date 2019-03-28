63a5a27b18bdea66e9831f4e256fb718dc5a6a4e
VovaLinuxMSU <vova@frolov.pp.ru>
2019-03-28 19:05:38 +0300

    edit

--- runmse.py
+++ runmse.py
@@ -35,45 +35,50 @@ def sorted_list_from_dir(folder1, ext):
   return sorted(ldr_files_by_time.items())
  
 
+def build_csv_table(imagefolder, csvfilename, algsuffix):
+  
+  ldr_dict_sorted1 = sorted_list_from_dir(imagefolder, ".png")
+  hdr_dict_sorted1 = sorted_list_from_dir(imagefolder, ".hdr")
+
+  ldr_ref = ldr_dict_sorted1[-1][1]
+  hdr_ref = hdr_dict_sorted1[-1][1]
+
+  ldr_ref2 = ldr_dict_sorted1[-2][1]
+  hdr_ref2 = hdr_dict_sorted1[-2][1]
+
+  refErrLdr = mse(imagefolder + "/" + ldr_ref, imagefolder + "/" + ldr_ref2)
+  refErrHdr = mse(imagefolder + "/" + hdr_ref, imagefolder + "/" + hdr_ref2)
+
+  csv_file = open(csvfilename, mode='w')
+  writer   = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+  arr1 = eval_mse_for_images(ldr_dict_sorted1, imagefolder)
+  arr2 = eval_mse_for_images(hdr_dict_sorted1, imagefolder)
+
+  writer.writerow(["time(min)", "mse_ldr" + algsuffix, "mse_ref_ldr", "time(min)", "256*mse_hdr" + algsuffix, "256*mse_ref_ldr"])
+
+  for i in range(0,len(arr1)):
+    (name1,err1) = arr1[i]
+    (name2,err2) = arr2[i]
+    time1 = int(re.search(r'\d+', name1).group())
+    time2 = int(re.search(r'\d+', name2).group())
+
+    if time1 == 0:
+      time1 = 0.5
+
+    if time2 == 0:
+      time2 = 0.5
+
+    writer.writerow([time1, err1, refErrLdr, time2, 256.0*err2, 256.0*refErrHdr])
+
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-INPUTFOLDER = "/home/frol/Dropbox/temp/L1.1/Hydra/PT/exp1"
 
-ldr_dict_sorted1 = sorted_list_from_dir(INPUTFOLDER, ".png")
-hdr_dict_sorted1 = sorted_list_from_dir(INPUTFOLDER, ".hdr")
-
-ldr_ref = ldr_dict_sorted1[-1][1]
-hdr_ref = hdr_dict_sorted1[-1][1]
-
-ldr_ref2 = ldr_dict_sorted1[-2][1]
-hdr_ref2 = hdr_dict_sorted1[-2][1]
-
-refErrLdr = mse(INPUTFOLDER + "/" + ldr_ref, INPUTFOLDER + "/" + ldr_ref2)
-refErrHdr = mse(INPUTFOLDER + "/" + hdr_ref, INPUTFOLDER + "/" + hdr_ref2)
-
-csv_file = open('out.csv', mode='w')
-writer   = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-arr1 = eval_mse_for_images(ldr_dict_sorted1, INPUTFOLDER)
-arr2 = eval_mse_for_images(hdr_dict_sorted1, INPUTFOLDER)
-
-
-writer.writerow(["time(min)", "mse_ldr", "mse_ref_ldr", "time(min)", "mse_hdr*256", "mse_ref_ldr*256"])
-
-for i in range(0,len(arr1)):
-  (name1,err1) = arr1[i]
-  (name2,err2) = arr2[i]
-  time1 = int(re.search(r'\d+', name1).group())
-  time2 = int(re.search(r'\d+', name2).group())
-
-  if time1 == 0:
-    time1 = 0.5
-
-  if time2 == 0:
-    time2 = 0.5
-
-  writer.writerow([time1, err1, refErrLdr, time2, 256.0*err2, 256.0*refErrHdr])
+build_csv_table("/home/frol/Dropbox/temp/L1.2/Hydra/PT/exp1",     "out_pt.csv",     "_pt")   
+build_csv_table("/home/frol/Dropbox/temp/L1.2/Hydra/PT_QMC/exp1", "out_pt_qmc.csv", "_pt_qmc")
+build_csv_table("/home/frol/Dropbox/temp/L1.2/Hydra/IBPT/exp1",   "out_ibpt.csv",   "_ibpt")
+build_csv_table("/home/frol/Dropbox/temp/L1.2/Hydra/MMLT/exp1",   "out_mmlt.csv",   "_mmlt")
 
