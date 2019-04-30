aea363d6d4b08da4ea4dc820dc3bb21fcc233c9d
FROL256 <vova@frolov.pp.ru>
2019-04-30 22:23:41 +0300

    tex shit

--- runcut.py
+++ runcut.py
@@ -6,7 +6,8 @@ import subprocess
 from os import listdir
 from os.path import isfile, join
 
-import csv
+import fileinput
+import io
 
 def mse(img1, img2):
   result = subprocess.run(["./imagemse", img1, img2], stdout=subprocess.PIPE)
@@ -30,7 +31,7 @@ def sorted_list_from_dir(folder1, ext):
   return sorted(ldr_files_by_time.items())
 
 
-def list_dirs(path):
+def list_dirs_deep(path):
   dirs = []
   for root, subdirs, files in os.walk(path):
     for file in os.listdir(root):
@@ -40,48 +41,93 @@ def list_dirs(path):
   return dirs
         
 
+class TestCase():
+  def __init__(self, a_folder, a_timePoint, a_bMinX, a_bMinY):
+    self.folder  = a_folder
+    self.timept  = a_timePoint
+    self.boxMinX = a_bMinX
+    self.boxMinY = 1024 - a_bMinY
 
-boxMinX_input = 295
-boxMinY_input = 1024 - 935
-
-timepoint     = 3 
-orb_path      = "/media/frol/886234F06234E49A/yandexdisk/2019/orb_2019_images/L1.2"
-
-mydirs = list_dirs(orb_path)
-mydirs = sorted(mydirs[4:]) # discard first 4 folders due to they are just render system folders, does not contain images
+    splitted    = a_folder.split(os.sep)
+    self.engine = splitted[-2]
+    self.method = splitted[-1]
+    self.outdir = splitted[-3]
 
 
-counter = 0
-for folder in mydirs:
-  splitted          = folder.split(os.sep)
-  (renderer,method) = (splitted[-2], splitted[-1])
-  allimages         = sorted_list_from_dir(folder, ".png")
-  targetimage       = allimages[timepoint][1]
-  cut(folder + "/" + targetimage, "outcuts/{0}_{1}_{2}.png".format(counter,renderer,method), boxMinX_input, boxMinY_input, 100, 100)
-  counter = counter+1
+def run_cut(orb_path, timepoint, boxMinX_input, boxMinY_input):
+  
+  images = []
 
-"""
+  mydirs = list_dirs_deep(orb_path)
+  mydirs = sorted(mydirs[4:]) # discard first 4 folders due to they are just render system folders, does not contain images
 
-images_hydra_pt   = sorted_list_from_dir(orb_path + "Hydra/PT",     ".png")
-images_hydra_qmc  = sorted_list_from_dir(orb_path + "Hydra/PT_QMC", ".png")
-images_hydra_ibpt = sorted_list_from_dir(orb_path + "Hydra/IBPT",   ".png")
-images_hydra_mmlt = sorted_list_from_dir(orb_path + "Hydra/MMLT",   ".png")
+  for folder in mydirs:
+    testc     = TestCase(folder, timepoint, boxMinX_input, boxMinY_input);
+    allimages = sorted_list_from_dir(folder, ".png")
+    if len(allimages) < 1:
+      continue
+    targetimage = allimages[timepoint][1]
+    outFolder   = testc.outdir + "_out";
+    if not os.path.exists(outFolder):
+      os.mkdir(outFolder)
+    outFileName = outFolder + "/{0}_{1}.png".format(testc.engine, testc.method)
+    cut(folder + "/" + targetimage, outFileName, testc.boxMinX, testc.boxMinY, 100, 100)
+    images.append(outFileName)
+  return images
 
-images_octane_pmc = sorted_list_from_dir(orb_path + "Octane/PMC",   ".png") 
-images_vray_pt_lc = sorted_list_from_dir(orb_path + "VRay/BF_LC",   ".png") 
+def array_to_matrix(inputArr, colNum):
+  inputArr2 = inputArr[:]
+  result    = []
 
-images_corona_pt_hd = sorted_list_from_dir(orb_path + "Corona/PT_UHD",   ".png") 
-images_corona_vcm   = sorted_list_from_dir(orb_path + "Corona/VCM",   ".png") 
+  while len(inputArr2) > 0:
+    result.append(inputArr2[:colNum])
+    inputArr2 = inputArr2[colNum:]
+  return result
 
-cut(orb_path + "Hydra/PT"     + "/" + images_hydra_pt[timepoint][1],   "outcuts/01_cut_hydra_pt.png",     boxMinX_input, boxMinY_input, 100, 100)
-cut(orb_path + "Hydra/PT_QMC" + "/" + images_hydra_qmc[timepoint][1],  "outcuts/02_cut_hydra_pt_qmc.png", boxMinX_input, boxMinY_input, 100, 100)
-cut(orb_path + "Hydra/IBPT"   + "/" + images_hydra_ibpt[timepoint][1], "outcuts/03_cut_hydra_ibpt.png",   boxMinX_input, boxMinY_input, 100, 100)
-cut(orb_path + "Hydra/MMLT"   + "/" + images_hydra_mmlt[timepoint][1], "outcuts/03_cut_hydra_mmlt.png",   boxMinX_input, boxMinY_input, 100, 100)
-cut(orb_path + "Octane/PMC"   + "/" + images_octane_pmc[timepoint][1], "outcuts/04_octane_pmc.png",       boxMinX_input, boxMinY_input, 100, 100)
 
-cut(orb_path + "VRay/BF_LC"   + "/" + images_vray_pt_lc[timepoint][1], "outcuts/05_vray_pt_lc.png",       boxMinX_input, boxMinY_input, 100, 100)
+#\includegraphics[width=0.23\linewidth]{01_glossy_cornell1_cut/zcut_1_LDR_PT_10min.png}
 
-cut(orb_path + "Corona/PT_UHD" + "/" + images_corona_pt_hd[timepoint][1], "outcuts/06_corona_pt_hd.png",  boxMinX_input, boxMinY_input, 100, 100)
-cut(orb_path + "Corona/VCM"    + "/" + images_corona_vcm[timepoint][1],   "outcuts/07_corona_vcm.png",    boxMinX_input, boxMinY_input, 100, 100)
+myimages1   = run_cut("/media/frol/886234F06234E49A/yandexdisk/2019/orb_2019_images/L1.1", 3, 265, 950)
+#myimages1_g = ["\\includegraphics[width=0.24\\linewidth]{" + fname + "}" for fname in myimages1]
+myimages2   = array_to_matrix(myimages1, 4)
 
-"""
+tabstrPrinter = io.StringIO()
+print("\\begin{tabular}{c c c c}", file=tabstrPrinter)
+
+for lineId in range(0, len(myimages2)):
+  line = myimages2[lineId]
+  
+  for columnId in range(0, len(line)):
+    column = line[columnId]
+    print("\\includegraphics[width=0.24\\linewidth]{" + column +"}", file=tabstrPrinter, end='')
+    if columnId != len(line)-1:
+      print(" & ", file=tabstrPrinter)
+  
+  print("\\\\", file=tabstrPrinter)
+  for columnId in range(0, len(line)):
+    column = line[columnId].split(os.sep)[1]
+    name   = column.replace("_", "\\_")
+    name   = name.replace(".png","")
+    print(name, file=tabstrPrinter, end='')
+    if columnId != len(line)-1:
+      print(" & ", file=tabstrPrinter)
+  
+  if lineId != len(myimages2)-1:    
+    print("\\\\", file=tabstrPrinter)
+  
+  print("", file=tabstrPrinter)
+
+print("", file=tabstrPrinter)
+print("\\end{tabular}", file=tabstrPrinter)
+
+
+
+
+
+tabstr = tabstrPrinter.getvalue()
+
+for line in fileinput.FileInput("zz_comp.tex", inplace=1):
+  if "REPLACEME" in line:
+    line = line.replace(line, tabstr)
+  print(line, end='')
+
